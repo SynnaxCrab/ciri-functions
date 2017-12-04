398156d2f63c99737e34295297924497368f709c
Jeff Lai <winfield301@gmail.com>
2017-12-04 21:41:50 +0800

    Add database config

--- functions/db-migrations/index.js
+++ functions/db-migrations/index.js
@@ -1,5 +1,12 @@
 console.log('starting function')
+
+require('dotenv').config()
+
+const DBM = require('db-migrate')
+const dbm = DBM.getInstance(true)
+
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
+  dbm.reset()
   cb(null, { hello: 'world' })
 }
