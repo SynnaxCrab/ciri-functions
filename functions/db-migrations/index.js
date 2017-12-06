e0e869809f112976141fe8a6b74213fa22f2c1c1
Jeff Lai <winfield301@gmail.com>
2017-12-06 16:01:56 +0800

    Add rollback support

--- functions/db-migrations/index.js
+++ functions/db-migrations/index.js
@@ -7,8 +7,17 @@ const dbm = DBM.getInstance(true)
 
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
-  dbm.up().then(() => {
-    console.log("successfully migrated")
-    cb(null, { status: 'done' })
-  })
+
+  switch (e.action) {
+    case 'up':
+      dbm.up().then(() => {
+        console.log("successfully migrated up")
+        cb(null, { status: 'up done' })
+      })
+    case 'down':
+      dbm.down(1).then(() => {
+        console.log("successfully migrated down")
+        cb(null, { status: 'down done' })
+      })
+  }
 }
