22e1b8a7d56a1837661b95bbf37f34629890457b
Jeff Lai <winfield301@gmail.com>
2017-12-06 16:31:34 +0800

    Explicit set dbm env to postgresql

--- functions/db-migrations/index.js
+++ functions/db-migrations/index.js
@@ -3,7 +3,9 @@ console.log('starting function')
 require('dotenv').config()
 
 const DBM = require('db-migrate')
-const dbm = DBM.getInstance(true)
+const dbm = DBM.getInstance(true, {
+  env: 'postgresql'
+})
 
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
