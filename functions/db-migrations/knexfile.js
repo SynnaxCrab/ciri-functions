2d83e7adaf13f71b2d196f42ffba0a98830ceb86
Jeff Lai <winfield301@gmail.com>
2018-04-18 21:44:56 +0800

    Remove duplicated config in knexfile.js

--- functions/db-migrations/knexfile.js
+++ functions/db-migrations/knexfile.js
@@ -1,42 +1,20 @@
 require('dotenv').config()
 
+defaultConfig = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+  },
+  migrations: {
+    tableName: 'migrations',
+  },
+}
+
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-    },
-    migrations: {
-      tableName: 'migrations',
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-    },
-    migrations: {
-      tableName: 'migrations',
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-    },
-    migrations: {
-      tableName: 'migrations',
-    },
-  },
+  development: defaultConfig,
+  staging: defaultConfig,
+  production: defaultConfig,
 }
