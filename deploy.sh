846f5a542079e59eecf33349ced9728fd80755f7
Jeff Lai <winfield301@gmail.com>
2018-04-18 18:03:36 +0800

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo PGUSER=$PGUSER >> ./functions/db-migrations/.env
 echo PGPASSWORD=$PGPASSWORD >> ./functions/db-migrations/.env
 echo PGDATABASE=$PGDATABASE >> ./functions/db-migrations/.env
 ./bin/apex deploy
-echo -n '{ "action": "up", migration: {} }' | ./bin/apex invoke db-migrations --logs
+echo -n '{ "action": "up", "migration": {} }' | ./bin/apex invoke db-migrations --logs
